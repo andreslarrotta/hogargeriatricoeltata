dbc69e66a00d1d091eb97f048710f7bd918ed132
Cesar Cruz <cc.cruz.caceres@gmail.com>
2017-11-17 00:26:28 -0500

    Added and configured email

---
+++ email/template.php
@@ -0,0 +1,34 @@
+<table cellspacing="0" cellpadding="0" border="0" style="color:#333;background:#fff;padding:0;margin:0;width:100%;font:15px/1.25em 'Helvetica Neue',Arial,Helvetica">
+    <tbody>
+        <tr width="100%">
+            <td valign="top" align="left" style="background:#eef0f1;font:15px/1.25em 'Helvetica Neue',Arial,Helvetica">
+                <table style="border:none;padding:0 18px;margin:50px auto;width:500px">
+                    <tbody>
+                        <tr width="100%" height="60">
+                            <td valign="top" align="left" style="border-top-left-radius:4px;border-top-right-radius:4px;background:#27709b url(https://ci5.googleusercontent.com/proxy/EX6LlCnBPhQ65bTTC5U1NL6rTNHBCnZ9p-zGZG5JBvcmB5SubDn_4qMuoJ-shd76zpYkmhtdzDgcSArG=s0-d-e1-ft#https://trello.com/images/gradient.png) bottom left repeat-x;padding:10px 18px;text-align:center"> <img height="40" width="125" src="/img/logo.png" title="El Tata" style="font-weight:bold;font-size:18px;color:#fff;vertical-align:top" class="CToWUd"> </td>
+                        </tr>
+                        <tr width="100%">
+                            <td valign="top" align="left" style="background:#fff;padding:18px">
+                                <h1 style="font-size:20px;margin:16px 0;color:#333;text-align:center"> Contacto </h1>
+                                <p style="font:15px/1.25em 'Helvetica Neue',Arial,Helvetica;color:#333;text-align:center"> Tienes un nuevo mensaje desde la página web: </p>
+                                <div style="background:#f6f7f8;border-radius:3px">
+                                    <br>
+                                    <p style="text-align:center"> <a href="#" style="color:#306f9c;font:26px/1.25em 'Helvetica Neue',Arial,Helvetica;text-decoration:none;font-weight:bold" target="_blank">{name}</a> </p>
+                                    <p style="font:15px/1.25em 'Helvetica Neue',Arial,Helvetica;margin-bottom:0;text-align:center">
+                                    	{email}
+                                    	<br>
+                                    	{phone}
+                                    	<br><br>
+                                    	{message}
+                                    </p>
+                                    <br>
+                                    <br> </div>
+                                <p style="font:14px/1.25em 'Helvetica Neue',Arial,Helvetica;color:#333"> <strong>Importante:</strong> No olvides responder las preguntas de tus contactos. </p>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
+            </td>
+        </tr>
+    </tbody>
+</table>
---
+++ js/contacto.js
@@ -0,0 +1,36 @@
+$(document).ready(function() {
+	var form = $('.contact-form');
+
+	form.formValidation({
+			framework: 'bootstrap',
+			locale: 'es_ES',
+			excluded: ':disabled'
+		})
+		.on('success.form.fv', function(e) {
+
+			e.preventDefault();
+
+			var $form = $(e.target);
+
+			$.ajax({
+				url: '/email/email.php',
+				type: 'POST',
+				data: $form.serialize(),
+				dataType: 'json',
+				success: function(response) {
+					
+					$form.formValidation('resetForm', true);
+
+					response.result === 'error' ? $('#alertContainer')
+						.removeClass('alert-success')
+						.addClass('alert-warning')
+						.html('Lo sentimos, no se logro enviar tu mensaje.')
+						.show() : $('#alertContainer')
+						.removeClass('alert-warning')
+						.addClass('alert-success')
+						.html('Tu mensaje ha sido enviado con éxito, pronto nos contactaremos con tigo.')
+						.show();
+				}
+			});
+		});
+});
